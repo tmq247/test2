929ff09fe4296d3b898812ccf313b4e404658141
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-24 02:04:26 +0700

    Update sudoers.py

--- wbb/modules/sudoers.py
+++ wbb/modules/sudoers.py
@@ -268,10 +268,8 @@ async def unban_globally(_, message):
     if not is_fmuted:
         await message.reply_text("I don't remember Fmuting him.")
     else:
-        served_chats = await get_served_chats()
-        served_chat in served_chats
         await remove_fmute_user(user.id)
-        await app.set_chat_permissions(served_chat["chat_id"], user_id, permissions=ChatPermissions())
+        await message.chat.unban_member(served_chat["chat_id"], user_id, permissions=ChatPermissions())
         await message.reply_text(f"{user.mention}'s unmuted.'")
 
 
