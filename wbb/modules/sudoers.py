e95213b1bd83495c5131612865cfe0b620ef0d14
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-24 01:22:29 +0700

    Update sudoers.py

--- wbb/modules/sudoers.py
+++ wbb/modules/sudoers.py
@@ -269,7 +269,6 @@ async def unmute_globally(_, message):
         await message.reply_text("I don't remember Fmuting him.")
     else:
         await remove_fmute_user(user.id)
-        await app.set_chat_permissions(chat_id, ChatPermissions())
         await message.reply_text(f"{user.mention}'s unmuted.'")
 
 
