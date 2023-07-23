24342ddcde5abbbc2dd26422def97875c1e95d67
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-24 02:27:28 +0700

    Update sudoers.py

--- wbb/modules/sudoers.py
+++ wbb/modules/sudoers.py
@@ -182,7 +182,7 @@ async def unban_globally(_, message):
         await message.reply_text("I don't remember Gbanning him.")
     else:
         await remove_gban_user(user.id)
-        await message.chat.unban_member(user_id)
+        await message.chat.unban_member(user_id, chat_id)
         await message.reply_text(f"Lifted {user.mention}'s Global Ban.'")
 
 
