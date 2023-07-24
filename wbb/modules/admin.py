be9067525313d62363826ddd6abb57c209381295
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-25 00:14:20 +0700

    Update admin.py

--- wbb/modules/admin.py
+++ wbb/modules/admin.py
@@ -489,7 +489,7 @@ async def promoteFunc(_, message: Message):
             can_change_info=False,
             can_invite_users=bot.can_invite_users,
             can_delete_messages=bot.can_delete_messages,
-            can_restrict_members,
+            can_restrict_members=False,
             can_pin_messages=False,
             can_promote_members=False,
             can_manage_chat=bot.can_manage_chat,
