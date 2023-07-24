ba50f344b4ab4cd613f0e534031c7115deb0e494
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-25 01:26:51 +0700

    Update __init__.py

--- wbb/__init__.py
+++ wbb/__init__.py
@@ -62,26 +62,26 @@ class Log:
         print(f"[+]: {msg}")
         if self.save_to_file:
             with open(self.file_name, "a") as f:
-                f.write(f"[INFO]({time.ctime(time.time())}): {msg}\n")
+                f.write(f"[THÔNG TIN]({time.ctime(time.time())}): {msg}\n")
 
     def error(self, msg):
         print(f"[-]: {msg}")
         if self.save_to_file:
             with open(self.file_name, "a") as f:
-                f.write(f"[ERROR]({time.ctime(time.time())}): {msg}\n")
+                f.write(f"[LỖI]({time.ctime(time.time())}): {msg}\n")
 
 
 log = Log(True, "bot.log")
 
 # MongoDB client
-log.info("Initializing MongoDB client")
+log.info("Đang khởi tạo ứng dụng khách MongoDB")
 mongo_client = MongoClient(MONGO_URL)
 db = mongo_client.wbb
 
 
 async def load_sudoers():
     global SUDOERS
-    log.info("Loading sudoers")
+    log.info("Đang tải sudoers")
     sudoersdb = db.sudoers
     sudoers = await sudoersdb.find_one({"sudo": "sudo"})
     sudoers = [] if not sudoers else sudoers["sudoers"]
@@ -120,12 +120,12 @@ arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
 
 app = Client("wbb", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
 
-log.info("Starting bot client")
+log.info("Bắt đầu máy khách bot")
 app.start()
-log.info("Starting userbot client")
+log.info("Bắt đầu ứng dụng khách userbot")
 app2.start()
 
-log.info("Gathering profile info")
+log.info("Thu thập thông tin hồ sơ")
 x = app.get_me()
 y = app2.get_me()
 
@@ -144,7 +144,7 @@ USERBOT_DC_ID = y.dc_id
 if USERBOT_ID not in SUDOERS:
     SUDOERS.add(USERBOT_ID)
 
-log.info("Initializing Telegraph client")
+log.info("Đang khởi tạo máy khách Telegraph")
 telegraph = Telegraph(domain="graph.org")
 telegraph.create_account(short_name=BOT_USERNAME)
 
