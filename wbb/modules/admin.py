12541032bee26a8463842014c2373f2a19457d8f
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-24 23:06:17 +0700

    Update admin.py

--- wbb/modules/admin.py
+++ wbb/modules/admin.py
@@ -624,11 +624,10 @@ async def unmute(_, message: Message):
 # Fmute
 
 
-@app.on_message(filters.command("m") & ~filters.list_admins)
+@app.on_message(filters.command("m") & ~filters.private)
 @adminsOnly("can_restrict_members")
-@capture_err
+#@capture_err
 async def mute_globally(_, message):
-    #permissions = "can_restrict_members"
     user_id, reason = await extract_user_and_reason(message)
     user = await app.get_users(user_id)
     from_user = message.from_user
@@ -693,11 +692,10 @@ __**New Global Mute**__
 # Unfmute
 
 
-@app.on_message(filters.command("um") & ~filters.list_admins)
+@app.on_message(filters.command("um") & ~filters.private)
 @adminsOnly("can_restrict_members")
-@capture_err
+#@capture_err
 async def unmute_globally(_, message):
-    #permissions = "can_restrict_members"
     user_id = await extract_user(message)
     from_user = message.from_user
     if not user_id:
