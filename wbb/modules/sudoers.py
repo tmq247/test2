6427b0c5d1adb77f9d95a276c9a0c1bfb0d5840b
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-24 19:00:18 +0700

    Update sudoers.py

--- wbb/modules/sudoers.py
+++ wbb/modules/sudoers.py
@@ -196,7 +196,7 @@ async def unban_globally(_, message):
 @app.on_message(filters.command("m") & SUDOERS)
 @capture_err
 async def mute_globally(_, message):
-    permissions = "can_restrict_members"
+    #permissions = "can_restrict_members"
     user_id, reason = await extract_user_and_reason(message)
     user = await app.get_users(user_id)
     from_user = message.from_user
@@ -264,7 +264,7 @@ __**New Global Mute**__
 @app.on_message(filters.command("um") & SUDOERS)
 @capture_err
 async def unmute_globally(_, message):
-    permissions = "can_restrict_members"
+    #permissions = "can_restrict_members"
     user_id = await extract_user(message)
     from_user = message.from_user
     if not user_id:
