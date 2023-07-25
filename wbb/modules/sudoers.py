097617a6bf416c0fba149fe9c4375a3850f01193
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-25 10:03:57 +0700

    Update sudoers.py

--- wbb/modules/sudoers.py
+++ wbb/modules/sudoers.py
@@ -114,17 +114,17 @@ async def ban_globally(_, message):
     from_user = message.from_user
 
     if not user_id:
-        return await message.reply_text("I can't find that user.")
+        return await message.reply_text("Tôi không thể tìm thấy người dùng này.")
     if not reason:
-        return await message.reply("No reason provided.")
+        return await message.reply("Không có lý do cung cấp.")
 
     if user_id in [from_user.id, BOT_ID] or user_id in SUDOERS:
-        return await message.reply_text("I can't ban that user.")
+        return await message.reply_text("ITôi không thể cấm người dùng này.")
 
     served_chats = await get_served_chats()
     m = await message.reply_text(
-        f"**Banning {user.mention} Globally!**"
-        + f" **This Action Should Take About {len(served_chats)} Seconds.**"
+        f"**Đang cấm {user.mention} trên toàn hệ thống!**"
+        + f" **Hành động này sẽ mất khoảng {len(served_chats)} giây.**"
     )
     await add_gban_user(user_id)
     number_of_chats = 0
@@ -140,20 +140,20 @@ async def ban_globally(_, message):
     try:
         await app.send_message(
             user.id,
-            f"Hello, You have been globally banned by {from_user.mention},"
-            + " You can appeal for this ban by talking to him.",
+            f"Xin chào, Bạn đã bị cấm trên toàn hệ thống bởi {from_user.mention},"
+            f" Bạn có thể khiếu nại lệnh cấm này bằng cách nói chuyện với {from_user.mention}.",
         )
     except Exception:
         pass
-    await m.edit(f"Banned {user.mention} Globally!")
+    await m.edit(f"Đã cấm {user.mention} trên toàn hệ thống!")
     ban_text = f"""
-__**New Global Ban**__
-**Origin:** {message.chat.title} [`{message.chat.id}`]
-**Admin:** {from_user.mention}
-**Banned User:** {user.mention}
-**Banned User ID:** `{user_id}`
-**Reason:** __{reason}__
-**Chats:** `{number_of_chats}`"""
+__**Người dùng bị cấm trên toàn hệ thống**__
+**Tại:** {message.chat.title} [`{message.chat.id}`]
+**Quản trị viên:** {from_user.mention}
+**Người dùng bị cấm:** {user.mention}
+**ID người dùng bị cấm:** `{user_id}`
+**Lý do:** __{reason}__
+**Số nhóm:** `{number_of_chats}`"""
     try:
         m2 = await app.send_message(
             GBAN_LOG_GROUP_ID,
@@ -161,12 +161,12 @@ __**New Global Ban**__
             disable_web_page_preview=True,
         )
         await m.edit(
-            f"Banned {user.mention} Globally!\nAction Log: ",
+            f"""Đã cấm {user.mention} trên toàn hệ thống bởi {from_user.mention}!!!""",
             disable_web_page_preview=True,
         )
     except Exception:
         await message.reply_text(
-            "User Gbanned, But This Gban Action Wasn't Logged, Add Me In GBAN_LOG_GROUP"
+            "Đã cấm người này trên toàn hệ thống, nhưng hành động này không được ghi lại, hãy thêm tôi vào nhóm quản lý"
         )
 
 
@@ -178,16 +178,16 @@ __**New Global Ban**__
 async def unban_globally(_, message):
     user_id = await extract_user(message)
     if not user_id:
-        return await message.reply_text("I can't find that user.")
+        return await message.reply_text("Tôi không thể tìm thấy người dùng này.")
     user = await app.get_users(user_id)
 
     is_gbanned = await is_gbanned_user(user.id)
     if not is_gbanned:
-        await message.reply_text("I don't remember Gbanning him.")
+        await message.reply_text("Tôi không nhớ đã cấm người này trên toàn hệ thống.")
     else:
         await remove_gban_user(user.id)
         await message.chat.unban_member(user_id)
-        await message.reply_text(f"Lifted {user.mention}'s Global Ban.'")
+        await message.reply_text(f"Đã bỏ cấm {user.mention}.'")
 
 
 
@@ -234,11 +234,11 @@ async def update_restart(_, message):
     try:
         out = subprocess.check_output(["git", "pull"]).decode("UTF-8")
         if "Already up to date." in str(out):
-            return await message.reply_text("Its already up-to date!")
+            return await message.reply_text("Nó đã được cập nhật!")
         await message.reply_text(f"```{out}```")
     except Exception as e:
         return await message.reply_text(str(e))
-    m = await message.reply_text("**Updated with default branch, restarting now.**")
+    m = await message.reply_text("**Đã cập nhật với nhánh mặc định, đang khởi động lại.**")
     await restart(m)
 
 
