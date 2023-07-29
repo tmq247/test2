30e27eaec160b4b97dd272d8a905fe0f2398f6a9
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-30 06:04:04 +0700

    Update admin.py

--- wbb/modules/admin.py
+++ wbb/modules/admin.py
@@ -504,13 +504,13 @@ async def promoteFunc(_, message: Message):
     await message.chat.promote_member(
         user_id=user_id,
         privileges=ChatPrivileges(
-            can_change_info=bot.can_change_info=False,
+            can_change_info=False,
             can_invite_users=bot.can_invite_users,
             can_delete_messages=bot.can_delete_messages,
             can_restrict_members=False,
-            can_pin_messages=bot.can_pin_messages=False,
+            can_pin_messages=False,
             can_promote_members=False,
-            can_manage_chat=bot.can_manage_chat=False,
+            can_manage_chat=False,
             can_manage_video_chats=bot.can_manage_video_chats,
         ),
     )
