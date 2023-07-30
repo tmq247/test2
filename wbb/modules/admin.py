45c93f43619a5bea7ba17b38ff9cc69f57791cc2
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-07-30 18:51:05 +0700

    Update admin.py

--- wbb/modules/admin.py
+++ wbb/modules/admin.py
@@ -741,10 +741,6 @@ async def mute_globally(_, message: Message):
         return await message.reply_text("Người này đã được xác nhận.")
         
     served_chats = await get_served_chats()
-    m = await message.reply_text(
-        f"**Đang cấm chat {user.mention} trên toàn hệ thống!**"
-        + f" **Hành động này sẽ mất khoảng {len(served_chats)} giây.**"
-    )
     await add_fmute_user(user_id)
     number_of_chats = 0
     for served_chat in served_chats:
